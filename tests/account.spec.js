cad48c258ac890ffdc472224c55c7b3bf8826034
Johannes Launer <johannes.launer@lusini.com>
2024-07-30 15:50:07 +0200

    refactor: Update account.spec.js for improved code organization and readability

--- tests/account.spec.js
+++ tests/account.spec.js
@@ -1,82 +1,70 @@
-import AccountPage from "../pages/AccountPage";
-import { secureClick } from "../helpers/Functions.js";
-import CustomCookies from "../helpers/Cookies";
+import { globalSetup } from "./fixtures/globalSetup"; // Adjust the import path as needed
+
 import config from "../playwright.config";
-const { chromium } = require("playwright");
 import { users } from "../data/users.json";
-import { test, expect } from "@playwright/test";
-import { beforeAll } from "@playwright/test";
-import { afterAll } from "@playwright/test";
+// import { test, expect } from "@playwright/test";
+import { test, expect } from "./fixtures/testObjects.js";
+import CustomCookies from "../helpers/Cookies";
 
-test.describe("template account", () => {
-  let browser, page, context, account, pageElements, customCookies, cookies;
-
-  beforeAll(async () => {
-    browser = await chromium.launch();
-    context = await browser.newContext();
-    page = await context.newPage();
-    account = new AccountPage(page); // Assuming 'page' is defined and is an instance of Playwright's Page
-    pageElements = account.elements;
-    customCookies = new CustomCookies(context);
-    // set Cookies
-    await customCookies.setB2c();
-    await customCookies.closeCookieBanner();
-    await customCookies.showAllCookies();
-  });
-
-  afterAll(async () => {
-    // Get all cookies
-    // const cookies = await context.cookies();
-    // console.log(cookies);
-    // console.log("AllCookies after afterAll:", cookies);
-    // await browser.close();
-  });
-
-  test("navigates to account-url when user click to the account-icon", async () => {
+test.describe("template accountPage", () => {
+  test("navigates to accountPage-url when user click to the accountPage-icon", async ({
+    accountPage,
+    page,
+  }) => {
     // make all single actions and assertions in the testfile
     await page.goto("/");
     // negative test
     await expect(page.url()).not.toBe(
-      config.use.baseURL + account.urls.accountLogin
+      config.use.baseURL + accountPage.urls.accountLogin
     );
-    // click on the account-icon
-    await account.actions.clickToIcon();
+    // click on the accountPage-icon
+    await accountPage.actions.clickToIcon();
     // wait for the accountpage
     // use assertions and their helper (pre)functions only in the testfile
-    await page.waitForURL(config.use.baseURL + account.urls.accountLogin, {
+    await page.waitForURL(config.use.baseURL + accountPage.urls.accountLogin, {
       timeout: 5000,
     });
     // expect to be on the accountpage
-    expect(page.url()).toBe(config.use.baseURL + account.urls.accountLogin);
+    expect(page.url()).toBe(config.use.baseURL + accountPage.urls.accountLogin);
   });
-  test("contains the account-context when user is on the account-page", async () => {
+  test("contains the accountPage-context when user is on the accountPage-page", async ({
+    accountPage,
+    page,
+  }) => {
     // negative test
-    await expect(pageElements.pageContext()).not.toBeVisible();
+    await expect(accountPage.elements.pageContext()).not.toBeVisible();
     // visit the accountpage
-    await page.goto(account.urls.account);
+    await page.goto(accountPage.urls.account);
     // locator exists and is visible on the page
-    await expect(pageElements.pageContext()).toBeVisible();
+    await expect(accountPage.elements.pageContext()).toBeVisible();
   });
-  test("contains a loginmask when user is on the account-page", async () => {
+  test("contains a loginmask when user is on the accountPage-page", async ({
+    accountPage,
+    page,
+  }) => {
     // negative test
-    await expect(pageElements.formLoginMail()).not.toBeVisible();
+    await expect(accountPage.elements.formLoginMail()).not.toBeVisible();
     // visit the accountpage
-    await page.goto(account.urls.accountLogin);
+    await page.goto(accountPage.urls.accountLogin);
     // locator exists and is visible on the page
-    await expect(pageElements.formLoginMail()).toBeVisible();
+    await expect(accountPage.elements.formLoginMail()).toBeVisible();
   });
-  test("get an error msg when login with an unregistered user", async () => {
+  test("get an error msg when login with an unregistered user", async ({
+    accountPage,
+    page,
+  }) => {
     // visit the accountpage
-    await page.goto(account.urls.accountLogin);
+    await page.goto(accountPage.urls.accountLogin);
+
     // negative test
-    await expect(pageElements.stateErrorPassword()).not.toBeVisible();
+    await expect(accountPage.elements.stateErrorPassword()).not.toBeVisible();
     // fill the login form
-    await account.actions.insertUserData(users[0]);
+    await accountPage.actions.insertUserData(users[0]);
     // submit the form
-    // await account.actions.clickLoginButton();
+    // await accountPage.actions.clickLoginButton();
     // wait for the error message 2 seconds
     // expect the error message to be visible
-    await expect(pageElements.stateErrorPassword()).toBeVisible({
+    await expect(accountPage.elements.stateErrorPassword()).toBeVisible({
       timeout: 2000,
     });
   });
